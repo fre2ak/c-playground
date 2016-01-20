ffa0fbdb81e214e25f414eb5e996a62ced343309
Ivo Dukov <fretwoak@gmail.com>
2016-01-20 23:34:42 +0200

    Reformat code.
    Signed-off-by: Ivo Dukov <fretwoak@gmail.com>

--- physics/ex2.c
+++ physics/ex2.c
@@ -2,19 +2,19 @@
 #include <math.h>
 
 double getDistance (double initialVelocity, double acceleration, double time) {
-	return (initialVelocity * time) + ((acceleration / 2) * pow(time, 2));
+    return (initialVelocity * time) + ((acceleration / 2) * pow(time, 2));
 }
 
 int main (int argc, char *argv[]) {
 	
-	double initialVelocity = 0,
-		acceleration = 10,
-		minutes = 2,
-		time = minutes * 60; // Convert minutes to seconds.
+    double initialVelocity = 0,
+        acceleration = 10,
+        minutes = 2,
+        time = minutes * 60; // Convert minutes to seconds.
 
-	double distance = getDistance(initialVelocity, acceleration, time);
+    double distance = getDistance(initialVelocity, acceleration, time);
 
-	printf("Distance is: %f m\n", distance);
+    printf("Distance is: %f m\n", distance);
 
-	return 0;
+    return 0;
 }
--- physics/ex3.c
+++ physics/ex3.c
@@ -2,37 +2,38 @@
 #include <math.h>
 
 double getAcceleration (double initialVelocity, double distance, double time) {
-	return (2 * distance / pow(time, 2)) - (2 * initialVelocity / time);
+    return (2 * distance / pow(time, 2)) - (2 * initialVelocity / time);
 }
 
 double getVelocity (double initialVelocity, double distance, double time) {
-	return (2 * distance / time) - initialVelocity;
+    return (2 * distance / time) - initialVelocity;
 }
 
 double getTime (double deceleration, double initialVelocity, double velocity) {
-	return deceleration * (velocity - initialVelocity);
+    return deceleration * (velocity - initialVelocity);
 }
 
 int main (int argc, char *argv[]) {
 
-	double	
-		initialVelocity = 0,
-		distance = 12100,
-		time = 110;	
+    double	
+        initialVelocity = 0,
+        distance = 12100,
+        time = 110;	
 
-	double acceleration = getAcceleration(initialVelocity, distance, time),
-		   deceleration = acceleration / 2;
+    double
+        acceleration = getAcceleration(initialVelocity, distance, time),
+        deceleration = acceleration / 2;
 
-	printf("Acceleration: %f m/s^2\n", acceleration);
-	printf("Deceleration: %f m/s^2\n", deceleration);
+    printf("Acceleration: %f m/s^2\n", acceleration);
+    printf("Deceleration: %f m/s^2\n", deceleration);
 
-	double velocity = getVelocity(initialVelocity, distance, time);
+    double velocity = getVelocity(initialVelocity, distance, time);
 
-	printf("Velocity: %f m/s\n", velocity);
+    printf("Velocity: %f m/s\n", velocity);
 
-	double timeToStop = getTime(deceleration, initialVelocity, velocity);
+    double timeToStop = getTime(deceleration, initialVelocity, velocity);
 
-	printf("Time to stop: %f s\n", timeToStop);
+    printf("Time to stop: %f s\n", timeToStop);
 
-	return 0;
+    return 0;
 }
